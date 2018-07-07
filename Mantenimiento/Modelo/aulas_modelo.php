416924370c8d81fb24935773180ba978c55e7165
HenTalavera <33740118+HenTalavera@users.noreply.github.com>
2018-07-07 04:18:56 -0500

    reparado - intento final

--- Mantenimiento/Modelo/aulas_modelo.php
+++ Mantenimiento/Modelo/aulas_modelo.php
@@ -19,26 +19,5 @@ class aulas_modelo{
 		return $this->aulast;
 	}
 }
-=======
-<?php
-class aulas_modelo{
 
-	private $db;
-	private $aulas;
-
-	public function __construct(){
-		require_once("conectar.php");
-		$this->db=conectar::conexion_db();
-		$this->docentes=array();
-	}
-
-	public function get_aulas(){
-		$aulast = array();
-		$result = $this->db->query("SELECT * FROM aula");
-		while($fila = $result->fetch_assoc()){
-			$this->aulast[]=$fila;
-		}
-		return $this->aulast;
-	}
-}
 ?>
